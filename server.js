42858f08458de637bdf9b5f8b64a3c2561a9d08a
Mr Sajad Abolpour Moshizi <MQ20189447@mq.edu.au>
2025-05-26 15:46:22 +1000

    Fix for Render: use HTTP server for WebSocket

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 // server.js
 const WebSocket = require('ws');
-const http = require('http');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
 
 const PORT = process.env.PORT || 10000;
-
-// Create HTTP server
-const server = http.createServer();
-
-// Attach WebSocket server to the HTTP server
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   console.log("Client connected");
 
-  ws.on('message', (message) => {
-    console.log("Received:", message.toString());
-    // Optional echo
-    // ws.send("ACK: " + message.toString());
+  ws.on('message', (msg) => {
+    console.log("Received:", msg.toString());
   });
 
   ws.on('close', () => {
@@ -24,7 +19,7 @@ wss.on('connection', (ws) => {
   });
 });
 
-// Start HTTP server
 server.listen(PORT, () => {
-  console.log(`WebSocket Server listening on port ${PORT}`);
+  console.log(`WebSocket Server running on port ${PORT}`);
 });
+
